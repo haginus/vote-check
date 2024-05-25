e339fa4561b647093305842629dc16533f1f21a1
Haginus <hagiandrei.ah@gmail.com>
2024-05-25 17:24:41 +0300

    migrate to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule, SetupGuard } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,7 +10,12 @@ import { MatCardModule } from '@angular/material/card';
 import { FormViewComponent } from './form-view/form-view.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatTableModule } from '@angular/material/table';
-import { DialogFormDelete, DialogFormExit, DialogFormSimpv, FormEditComponent } from './form-edit/form-edit.component';
+import {
+  DialogFormDelete,
+  DialogFormExit,
+  DialogFormSimpv,
+  FormEditComponent,
+} from './form-edit/form-edit.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -31,13 +34,15 @@ import { environment } from '../environments/environment';
 import { DialogInfo } from './dashboard/dashboard.component';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
 import { SettingsComponent } from './settings/settings.component';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { SetupComponent } from './setup/setup.component';
 import { NamePipe } from './name.pipe';
 import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
 import { ProgressComponent } from './tasks/progress/progress.component';
 import { TaskSectionComponent } from './tasks/task-section/task-section.component';
-
 
 @NgModule({
   declarations: [
@@ -55,11 +60,11 @@ import { TaskSectionComponent } from './tasks/task-section/task-section.componen
     NamePipe,
     TasksListComponent,
     ProgressComponent,
-    TaskSectionComponent
+    TaskSectionComponent,
   ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
@@ -81,9 +86,10 @@ import { TaskSectionComponent } from './tasks/task-section/task-section.componen
     MatTooltipModule,
     FormsModule,
     ReactiveFormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
-  providers: [SetupGuard],
-  bootstrap: [AppComponent]
+  providers: [SetupGuard, provideHttpClient(withInterceptorsFromDi())],
 })
-export class AppModule { }
+export class AppModule {}
