e6df67d27904b6c39106730500154f3447f8de0c
Haginus <hagiandrei.ah@gmail.com>
2024-05-27 23:16:37 +0300

    add candidates for the upcoming elections

--- src/elections/elections.ts
+++ src/elections/elections.ts
@@ -3,6 +3,11 @@ import { Election } from "./types";
 
 export const elections = [
   {
+    id: "locale27092020",
+    type: getElectionType('LOCAL'),
+    date: new Date("2020-09-27")
+  },
+  {
     id: "locale09062024",
     type: getElectionType('LOCAL'),
     date: new Date("2024-06-09")
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ import { getElection } from "../elections/elections";
 export const environment = {
   appVersion: "3.0.0" + '-dev',
   currentElections: [
+    getElection('locale27092020'), // We keep this for now as SIMPV is not live yet for the new elections
     getElection('locale09062024'),
     getElection('europarlamentare09062024'),
   ],
