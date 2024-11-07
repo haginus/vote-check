44bfab0abeaee73b8e5f342ffffdb5dd6b3b960e
Haginus <hagiandrei.ah@gmail.com>
2024-11-07 20:25:15 +0200

    allow selection precincts from abroad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 import { getElection } from "../elections/elections";
 
 export const environment = {
-  appVersion: "3.1.0",
+  appVersion: "3.2.0",
   currentElections: [
-    getElection('locale09062024'),
     getElection('europarlamentare09062024'),
+    getElection('locale09062024'),
   ],
   production: false
 };
